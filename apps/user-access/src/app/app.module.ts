6b717f74a57ee90e6e54cebcb079e54edb090e44
Hoa Nguyen <httt.dev@gmail.com>
2025-10-19 19:59:33 +0700

    feat/#userrole: add role of user

--- apps/user-access/src/app/app.module.ts
+++ apps/user-access/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CONFIGURATION, TConfiguration } from '../configuration';
+import { RoleModule } from './modules/role/role.module';
+import { MongoProvider } from '@common/configuration/mongo.config';
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true, load: [() => CONFIGURATION] })],
+    imports: [ConfigModule.forRoot({ isGlobal: true, load: [() => CONFIGURATION] }), MongoProvider, RoleModule],
     controllers: [],
     providers: [],
 })
